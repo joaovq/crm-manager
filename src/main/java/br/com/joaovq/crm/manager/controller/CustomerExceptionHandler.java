de69dd4575f7795b60090fe6432fe00a64a46dc3
joaovq <joaovqdev@gmail.com>
2023-12-12 16:48:09 -0300

    refactor: add new handler exception

--- src/main/java/br/com/joaovq/crm/manager/controller/CustomerExceptionHandler.java
+++ src/main/java/br/com/joaovq/crm/manager/controller/CustomerExceptionHandler.java
@@ -3,14 +3,14 @@ package br.com.joaovq.crm.manager.controller;
 import br.com.joaovq.crm.manager.core.exception.CustomerNotFoundException;
 import br.com.joaovq.crm.manager.core.utils.ExceptionHandlerResponse;
 import br.com.joaovq.crm.manager.data.service.CustomerService;
-import org.springframework.http.HttpHeaders;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomerExceptionHandler {
     @ExceptionHandler(CustomerNotFoundException.class)
     public ResponseEntity<ExceptionHandlerResponse> handleCustomerNotFound(
@@ -19,6 +19,19 @@ public class CustomerExceptionHandler {
                 new ExceptionHandlerResponse(
                         HttpStatus.BAD_REQUEST.value(),
                         "Customer not found! try again.",
+                        ex.toString(),
+                        CustomerService.class.toString()
+                ), HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionHandlerResponse> handleDataIntegrityViolation(
+            DataIntegrityViolationException ex, WebRequest request) {
+        return new ResponseEntity<>(
+                new ExceptionHandlerResponse(
+                        HttpStatus.BAD_REQUEST.value(),
+                        ex.getMessage(),
                         ex.toString(),
                         CustomerService.class.toString()
                 ), HttpStatus.BAD_REQUEST
