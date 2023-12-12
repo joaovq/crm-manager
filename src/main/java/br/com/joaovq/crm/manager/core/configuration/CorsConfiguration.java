a9586b0c372d16330c69f869e9eef1185ea695df
joaovq <joaovqdev@gmail.com>
2023-12-12 16:46:23 -0300

    feat: create cors configuration origins

---
+++ src/main/java/br/com/joaovq/crm/manager/core/configuration/CorsConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.joaovq.crm.manager.core.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfiguration {
+    @Value("${cors.origins}")
+    private String origins;
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry
+                        .addMapping("/v1/*")
+                        .allowedOrigins(origins.split(","));
+            }
+        };
+    }
+}
