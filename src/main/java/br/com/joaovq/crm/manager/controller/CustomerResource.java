8e1a298d8bc972e66f878610cfcc3e630bdfc7f9
joaovq <joaovqdev@gmail.com>
2023-12-12 16:48:45 -0300

    feat: add search routes resource customer

--- src/main/java/br/com/joaovq/crm/manager/controller/CustomerResource.java
+++ src/main/java/br/com/joaovq/crm/manager/controller/CustomerResource.java
@@ -5,23 +5,35 @@ import br.com.joaovq.crm.manager.data.service.CustomerUseCase;
 import br.com.joaovq.crm.manager.domain.request.CustomerCreateRequest;
 import br.com.joaovq.crm.manager.domain.response.CustomerResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "v1/customer")
 public class CustomerResource {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
     @Autowired
     private CustomerUseCase customerService;
 
+    @GetMapping(value = "search")
+    public ResponseEntity<Collection<CustomerResponse>> search(@RequestParam(name = "query", defaultValue = "") String name) {
+        var customers = customerService.searchCustomerByName(name);
+        return ResponseEntity.ok(customers.stream().map(CustomerResponse::toResponse).toList());
+    }
+
     @GetMapping
-    public ResponseEntity<Page<Customer>> getAllCustomers(Pageable pageable) {
+    public ResponseEntity<Page<Customer>> getAllCustomers(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(customerService.listAllCustomers(pageable));
     }
 
@@ -31,9 +43,10 @@ public class CustomerResource {
         return ResponseEntity.ok(CustomerResponse.toResponse(customer));
     }
 
-    @GetMapping("{id}")
+    @GetMapping(value = "{id}")
     public ResponseEntity<CustomerResponse> getById(@PathVariable UUID id) {
         Customer customer = customerService.getCustomerById(id);
         return ResponseEntity.ok(CustomerResponse.toResponse(customer));
     }
+
 }
